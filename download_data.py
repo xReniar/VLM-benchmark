63b4bcbd78c3c92afe3be247831aaa6aa946e036
xReniar <ilaorainer@gmail.com>
2025-06-23 02:18:38 +0200

    Add parse_args function

--- download_data.py
+++ download_data.py
@@ -1,4 +1,17 @@
 from datasets import load_dataset
+import argparse
 
 
-ds = load_dataset("nanonets/key_information_extraction")
+ds = load_dataset("nanonets/key_information_extraction", split="test")
+
+for x in ds:
+    print(x.keys())
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Dataset downloader",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+if __name__ == "__main__":
+    pass
