18ba69c76d3b2b81aa388ab44d7e4149c23e2c26
xReniar <ilaorainer@gmail.com>
2025-06-24 15:02:02 +0200

    Add predictor class for all the predictors

---
+++ vlm/predictor.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import torch
+import json
+
+
+class Predictor(ABC):
+    def __init__(
+        self
+    ) -> None:
+        super().__init__()
+
+        # init model and other stuff
+        self._init_model()
+
+    def device(self) -> str:
+        if torch.cuda.is_available():
+            return "cuda"
+        elif torch.mps.is_available():
+            return "mps"
+        else:
+            return "cpu"
+
+    @abstractmethod
+    def _init_model(self):
+        pass
+
+    @abstractmethod
+    def inference(self):
+        pass
