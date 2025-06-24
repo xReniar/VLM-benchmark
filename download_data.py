48dd34ffe0932aac300a5941c2634a2070212bd0
xReniar <ilaorainer@gmail.com>
2025-06-24 15:21:20 +0200

    Update download process for kie

--- download_data.py
+++ download_data.py
@@ -8,20 +8,20 @@ import inspect
 
 
 def kie():
-    output_path = os.path.join("data", "kie")
+    output_path = os.path.join("datasets", "kie")
+    os.makedirs(os.path.join(output_path, "images"), exist_ok=True)
+    os.makedirs(os.path.join(output_path, "labels"), exist_ok=True)
 
-    if not os.path.isdir(output_path):
-        os.makedirs(output_path, exist_ok=True)
-        for i, row in enumerate(load_dataset("nanonets/key_information_extraction", split="test")):
-            image_bytes = row["image"]
-            annotations = row["annotations"]
+    for i, row in enumerate(load_dataset("nanonets/key_information_extraction", split="test")):
+        image_bytes = row["image"]
+        annotations = row["annotations"]
 
-            with open(os.path.join(output_path, f"{i}.json"), "w") as file:
-                json.dump(annotations, file, indent=4)
+        with open(os.path.join(output_path, "labels", f"{i}.json"), "w") as file:
+            json.dump(annotations, file, indent=4)
 
-            image = Image.open(io.BytesIO(image_bytes))
-            image.save(os.path.join(output_path, f"{i}.png"))
-            image.close()
+        image = Image.open(io.BytesIO(image_bytes))
+        image.save(os.path.join(output_path, "images", f"{i}.png"))
+        image.close()
 
 
 def parse_args():
