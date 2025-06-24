fb51f5231e2da5cf5311531401693b9df7b82fa2
xReniar <ilaorainer@gmail.com>
2025-06-24 15:00:28 +0200

    Add ollama predictor class for inference

---
+++ vlm/OllamaPredictor.py
@@ -0,0 +1,12 @@
+from .predictor import Predictor
+
+
+class OllamaPredictor(Predictor):
+    def __init__(self):
+        super().__init__()
+
+    def _init_model(self):
+        pass
+
+    def predict(self):
+        pass
