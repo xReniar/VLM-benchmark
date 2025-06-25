3c7457540d804f13ee0fb2e88d40e505c6a62615
xReniar <ilaorainer@gmail.com>
2025-06-25 21:14:20 +0200

    Implement Smolvml inferencer

--- vlm/hugginface/smolvlm.py
+++ vlm/hugginface/smolvlm.py
@@ -1,43 +1,46 @@
-#from ..HuggingFacePredictor import HuggingFacePredictor
-
-
-import torch
-from transformers import AutoProcessor, AutoModelForImageTextToText, AutoModelForVision2Seq
+from ..HuggingFacePredictor import HuggingFacePredictor
 from PIL import Image
+import torch
 
 
-model_id = "HuggingFaceTB/SmolVLM2-500M-Video-Instruct"
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+# model_id = "HuggingFaceTB/SmolVLM2-500M-Video-Instruct"
+class SmolVLM(HuggingFacePredictor):
+    def __init__(
+        self,
+        model_name: str
+    ) -> None:
+        super().__init__(model_name)
 
-processor = AutoProcessor.from_pretrained(model_id)
-model = AutoModelForImageTextToText.from_pretrained(model_id,
-                                                torch_dtype=torch.bfloat16,
-                                                _attn_implementation="eager").to(DEVICE)
+    def inference(
+        self,
+        img_path: str,
+        prompt: str
+    ):
+        img = Image.open(img_path)
 
+        messages = [
+            {"role": "user", "content": [
+                {"type": "image", "image": img},
+                {"type": "text", "text": prompt},
+            ]}
+        ]
 
-def run_inference(image: Image.Image, prompt: str):
-    messages = [
-        {"role": "user", "content": [
-            {"type": "image", "image": image},
-            {"type": "text", "text": prompt},
-        ]}
-    ]
+        inputs = self.processor.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_dict=True,
+            return_tensors="pt",
+        ).to(self.model.device, dtype=torch.bfloat16)
 
-    #inputs = processor.apply_chat_template(messages, add_generation_prompt=True)
-    # Step 2: Process image + prompt text into model inputs
-    #model_inputs = processor(text=inputs, images=[image], return_tensors="pt").to(DEVICE)
+        outputs = self.model.generate(
+            **inputs,
+            do_sample=True,
+            temperature=0.1,
+            top_p=0.9,
+            top_k=0,
+            repetition_penalty=1.3,
+            max_new_tokens=1000
+        )
 
-    #outputs = model.generate(**model_inputs)
-
-
-    inputs = processor.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        tokenize=True,
-        return_dict=True,
-        return_tensors="pt",
-    ).to(model.device, dtype=torch.bfloat16)
-
-    outputs = model.generate(**inputs, do_sample=True, temperature=0.1, top_p=0.9, top_k=0, repetition_penalty=1.3, max_new_tokens=1000)
-
-    return processor.decode(outputs[0], skip_special_tokens=True)
+        return self.processor.decode(outputs[0], skip_special_tokens=True)
