e4ba84539c62f8cb4af519875c66b645dff99cdd
xReniar <ilaorainer@gmail.com>
2025-06-27 15:47:02 +0200

    Add unique function for calculating kie metrics

--- kie_metrics.py
+++ kie_metrics.py
@@ -9,11 +9,31 @@ gt_labels = sorted(os.listdir(gt_path))
 
 
 def get_kie_metrics(
-    predictions: list[str],
-    labels_fn: list[str]
-):
-    pass
+    vlm_name: str
+) -> float:
+    responses_path = f"responses/parsed/kie/{vlm_name}"
+    predictions = sorted(os.listdir(responses_path))
 
+    edit_distances = []
+    for (pred, gt) in zip(predictions, gt_labels):
+        pred_json:dict = json.load(open(os.path.join(responses_path, pred), "r"))
+        gt_json:dict = json.load(open(os.path.join(gt_path, gt), "r"))
+
+        for gt_field in gt_json.keys():
+            pred_value = pred_json.get(gt_field, "")
+
+            pred_value = str(pred_value)
+            gt_value = str(gt_json[gt_field])
+
+            dist = edit_distance(pred_value, gt_value)
+            max_len = max(len(pred_value), len(gt_value))
+            if max_len == 0:
+                edit_distances.append(1.0)
+            else:
+                edit_distances.append(1 - (dist / max_len))
+    return round(sum(edit_distances) / len(edit_distances), 3)
+
+'''
 def smolvlm():
     responses_path = "responses/parsed/kie/smolvlm"
     predictions = sorted(os.listdir(responses_path))
@@ -60,7 +80,8 @@ def qwen2_5_VL():
             else:
                 edit_distances.append(1 - (dist / max_len))
     return sum(edit_distances) / len(edit_distances)
+'''
 
 
-print(smolvlm())
-print(qwen2_5_VL())
+print(get_kie_metrics("smolvlm"))
+print(get_kie_metrics("qwen2.5"))
