1366c89b50b28ac2d0b0e41ea3d98a0e5984a77a
xReniar <ilaorainer@gmail.com>
2025-07-11 01:01:59 +0200

    Add predictor hyperparameters

--- vlm/predictors/predictors.py
+++ vlm/predictors/predictors.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from transformers import AutoProcessor
+from PIL import Image
 import torch
 
-
 class HFPredictor(ABC):
-    def __init__(self, model_name: str):
+    def __init__(self, model_name: str, args: dict):
         super().__init__()
         self.model_name = model_name
         self.device = torch.device(
@@ -13,17 +13,22 @@ class HFPredictor(ABC):
             "cpu"
         )
 
+        # predictor hyperparameters
+        self.top_p = float(args["top_p"])
+        self.top_k = float(args["top_k"])
+        self.max_tokens = int(args["max_tokens"])
+
         # instantiate model
         self.processor = AutoProcessor.from_pretrained(self.model_name)
         self._init_model()
 
-    def open_img(self, img_path: str):
-        pass
+    def open_img(self, img_path: str) -> Image.Image:
+        return Image.open(img_path)
 
     @abstractmethod
-    def _init_model(self):
+    def _init_model(self) -> None:
         pass
     
     @abstractmethod
-    def inference(self, prompt):
+    def inference(self, prompt: str, img_path: str):
         pass
