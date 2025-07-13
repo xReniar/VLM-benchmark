beb21f7d6f93df6e48f65b9e56b37eaef1dfc59a
xReniar <ilaorainer@gmail.com>
2025-07-13 19:14:54 +0200

    Delete old code

--- vlm/ollama/qwen2.5.py
+++
@@ -1,6 +0,0 @@
-from ..OllamaPredictor import OllamaPredictor
-
-
-class Qwen2_5(OllamaPredictor):
-    def __init__(self):
-        super().__init__()
--- vlm/predictors/__init__.py
+++
@@ -1,1 +0,0 @@
-from .qwen2_5 import Qwen2_5_VL
