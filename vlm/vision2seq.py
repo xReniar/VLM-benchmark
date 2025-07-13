68df40dd8ebc77f6fcd5e935516984caedbefe3b
xReniar <ilaorainer@gmail.com>
2025-07-13 18:11:46 +0200

    Implement Vision2Seq models

---
+++ vlm/vision2seq.py
@@ -0,0 +1,45 @@
+from .base import VLMModelBase
+from transformers import AutoModelForVision2Seq
+import torch
+
+
+@VLMModelBase.register_model("Vision2Seq")
+class Vision2Seq(VLMModelBase):
+    def __init__(self, config: dict):
+        super().__init__(config)
+
+    def _init_model(self):
+        return AutoModelForVision2Seq.from_pretrained(
+            self.config["model_id"],
+            torch_dtype=torch.bfloat16,
+            _attn_implementation="eager"
+        ).to(self.device)
+
+    def predict(self, img_path: str, prompt: str) -> str:
+        messages = [{
+            "role": "user",
+            "content": [
+                {"type": "image"},
+                {"type": "text", "text": prompt}
+            ]
+        }]
+
+        img = self.open_img(img_path)
+
+        prompt = self.processor.apply_chat_template(
+            messages,
+            add_generation_prompt=True
+        )
+        inputs = self.processor(
+            text=prompt,
+            images=img,
+            return_tensors="pt"
+        ).to(self.device)
+
+        generated_ids = self.model.generate(**inputs, max_new_tokens=500)
+        generated_texts = self.processor.batch_decode(
+            generated_ids,
+            skip_special_tokens=True,
+        )
+
+        return generated_texts[0]
