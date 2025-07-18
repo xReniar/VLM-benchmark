da9c18ae9fa4d90412f5504f8ed3bb2f5018cdd0
xReniar <ilaorainer@gmail.com>
2025-07-18 21:18:32 +0200

    Fix OCR task for DocILE

--- dataset/docile.py
+++ dataset/docile.py
@@ -3,7 +3,6 @@ from pdf2image import convert_from_path
 from PIL import Image
 import json
 import os
-import gc
 
 
 class DocILE(Dataset):
@@ -29,7 +28,6 @@ class DocILE(Dataset):
                 )
                 images[0].save(f"./data/docile/pdfs/{fn.strip('.pdf')}.jpg")
                 os.remove(f"./data/docile/pdfs/{fn}")
-        gc.collect()
 
     def _load_data(self) -> None:
         split_file: list[str] = json.load(open(f"./data/docile/{self.split}.json", "r"))
@@ -42,43 +40,44 @@ class DocILE(Dataset):
 
             if self.task == Task.OCR:
                 fields = []
-                img = Image.open(f"./data/docile/pdfs/{img_fn}.jpg")
-                for block in label["pages"][0]["blocks"]:
-                    for line in block["lines"]:
-                        for word in line["words"]:
-                            x1, y1 = word["geometry"][0]
-                            x2, y2 = word["geometry"][1]
 
-                            fields.append(self._convert_to_format(
-                                task = Task.OCR,
-                                item = dict(
-                                    bbox = [int(x1 * img.width), int(y1 * img.height), int(x2 * img.width), int(y2 * img.height)],
-                                    text = word["value"]
-                                )
-                            ))
+                if os.path.exists(f"./data/docile/pdfs/{img_fn}.jpg"):
+                    img = Image.open(f"./data/docile/pdfs/{img_fn}.jpg")
+                    for block in label["pages"][0]["blocks"]:
+                        for line in block["lines"]:
+                            for word in line["words"]:
+                                x1, y1 = word["geometry"][0]
+                                x2, y2 = word["geometry"][1]
 
-                self.data.append(Data(
-                    image_path=f"./data/docile/pdfs/{img_fn}.jpg",
-                    fields=fields
-                ))
+                                fields.append(self._convert_to_format(
+                                    task = Task.OCR,
+                                    item = dict(
+                                        bbox = [int(x1 * img.width), int(y1 * img.height), int(x2 * img.width), int(y2 * img.height)],
+                                        text = word["value"]
+                                    )
+                                ))
+
+                    self.data.append(Data(
+                        image_path=f"./data/docile/pdfs/{img_fn}.jpg",
+                        fields=fields
+                    ))
 
             if self.task == Task.KIE:
                 entities = []
 
-                if "field_extractions" in label:
-                    for extraction in label["field_extractions"]:
-                        entities.append(self._convert_to_format(
-                            task = Task.KIE,
-                            item = dict(
-                                label = extraction["fieldtype"],
-                                value = extraction["text"]
-                            )
-                        ))
-
-                    self.data.append(Data(
-                        image_path=f"./data/docile/pdfs/{img_fn}.jpg",
-                        entities=entities
+                for extraction in label["field_extractions"]:
+                    entities.append(self._convert_to_format(
+                        task = Task.KIE,
+                        item = dict(
+                            label = extraction["fieldtype"],
+                            value = extraction["text"]
+                        )
                     ))
+
+                self.data.append(Data(
+                    image_path=f"./data/docile/pdfs/{img_fn}.jpg",
+                    entities=entities
+                ))
 
 
 
