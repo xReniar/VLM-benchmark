7d9f92f10729db98d6e6499e422d31d830587793
xReniar <ilaorainer@gmail.com>
2025-07-18 14:23:53 +0200

    Update sample

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from vlm import VLM
+from dataset import Task, SROIE, DocILE
 
 
-x = VLM("SmolVLM")
-response = x.predict("datasets/kie/images/0.png", "How much do I need to pay?")
+#x = VLM("SmolVLM")
+#response = x.predict("datasets/kie/images/0.png", "How much do I need to pay?")
 
-print(response)
+
+for data in DocILE(task=Task.KIE, split="val"):
+    print(data)
