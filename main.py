5f177ac722635ea71f28600de700ed78443c8cbb
xReniar <ilaorainer@gmail.com>
2025-07-20 02:54:06 +0200

    Update test code

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from vlm import VLM
 from dataset import Task, SROIE, DocILE
 
 
-#x = VLM("SmolVLM")
-#response = x.predict("datasets/kie/images/0.png", "How much do I need to pay?")
+x = VLM("Qwen2.5-VL")
+response = x.predict("data/kie/images/0.png", "How much do I need to pay?")
 
+print(response)
+
+'''
 for data in DocILE(task=Task.OCR, split="train"):
     image_path = data["image_path"]
     for entity in data["fields"]:
         print(entity)
+'''
 
 '''
 for data in DocILE(task=Task.KIE, split="train"):
