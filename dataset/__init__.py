c97859e618550d276dd068d6d6fef96e8ae7ef8d
xReniar <ilaorainer@gmail.com>
2025-07-22 14:40:52 +0200

    Add MultiDataset class

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,11 +1,40 @@
-from .ds import Task
-
+from .ds import Task, Data, Dataset
 from .docile import DocILE
 from .sroie import SROIE
+
+
+dataset_map = {
+    "DocILE": DocILE,
+    "SROIE": SROIE
+}
+
+class MultiDataset():
+    def __init__(
+        self,
+        datasets: list[str],
+        task: list[Task],
+        split: str
+    ) -> None:
+        self.data: list[Data] = []
+        self._load_data(datasets, task, split)
+
+    def _get_data(self, task: Task, data: Data) -> list:
+        pass
+
+    def _load_data(
+        self,
+        datasets: list[str],
+        task: list[Task],
+        split: str
+    ) -> None:
+        for dataset in datasets:
+            dataset_obj: Dataset = dataset_map[dataset]
+            dataset_tasks: list[Task] = dataset_obj.TASKS
 
 
 __all__ = [
     "Task",
     "DocILE",
-    "SROIE"
+    "SROIE",
+    "MultiDataset"
 ]
