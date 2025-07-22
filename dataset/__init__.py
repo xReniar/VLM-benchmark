f31be43ced2a250228069bf4a1fe6519df27c363
xReniar <ilaorainer@gmail.com>
2025-07-22 16:07:59 +0200

    Finish implementation of MultiDataset

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,33 +3,28 @@ from .docile import DocILE
 from .sroie import SROIE
 
 
-dataset_map = {
-    "DocILE": DocILE,
-    "SROIE": SROIE
-}
-
 class MultiDataset():
     def __init__(
         self,
-        datasets: list[str],
-        task: list[Task],
+        selections: list[tuple[Dataset, list[Task]]],
         split: str
     ) -> None:
         self.data: list[Data] = []
-        self._load_data(datasets, task, split)
+        self._load_data(selections, split)
 
-    def _get_data(self, task: Task, data: Data) -> list:
-        pass
+    def __iter__(self):
+        return self.data.__iter__()
 
     def _load_data(
         self,
-        datasets: list[str],
-        task: list[Task],
+        selections: list[tuple[Dataset, list[Task]]],
         split: str
     ) -> None:
-        for dataset in datasets:
-            dataset_obj: Dataset = dataset_map[dataset]
-            dataset_tasks: list[Task] = dataset_obj.TASKS
+        for selection in selections:
+            dataset: Dataset = selection[0]
+            tasks: list[Task] = selection[1]
+
+            self.data += dataset(tasks,split).data
 
 
 __all__ = [
