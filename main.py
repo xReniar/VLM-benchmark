5906e32e8e235407c3bf67bb2578626ba590f43f
xReniar <ilaorainer@gmail.com>
2025-07-23 02:37:06 +0200

    Add more example tests

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from vlm import VLM
-from dataset import Task, SROIE, DocILE
+from dataset import Task, SROIE, DocILE, MultiDataset
 
 
-x = VLM("Qwen2.5-VL")
-response = x.predict("data/kie/images/0.png", "How much do I need to pay?")
+#x = VLM("Qwen2.5-VL")
+#response = x.predict("data/kie/images/0.png", "How much do I need to pay?")
 
-print(response)
+#print(response)
 
 '''
-for data in DocILE(task=Task.OCR, split="train"):
+for data in DocILE(tasks=[Task.KIE, Task.OCR], split="train"):
+    print(data)
     image_path = data["image_path"]
     for entity in data["fields"]:
         print(entity)
 '''
 
+for data in DocILE(tasks=[Task.OCR, Task.KIE], split="train"):
+    print(data)
+
+
 '''
-for data in DocILE(task=Task.KIE, split="train"):
+for data in MultiDataset([
+    #(SROIE, [Task.OCR]),
+    (DocILE, [Task.OCR])
+], "train"):
     print(data)
 '''
