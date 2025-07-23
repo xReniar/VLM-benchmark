15a4123fa7c5db08f58e481de77dafa7296e3e3e
xReniar <ilaorainer@gmail.com>
2025-07-23 17:00:21 +0200

    Create inference script

---
+++ inference.py
@@ -0,0 +1,18 @@
+from vlm import VLM
+import yaml
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    benchmark_config = yaml.safe_load(open("./configs/benchmark.yaml", "r"))
+    test_config = benchmark_config["test"]
+    tasks_config = benchmark_config["tasks"]
+
+    TASK: str = benchmark_config["task"]
+    DATASET: str = benchmark_config["dataset"]
+    MODELS: list[str] = benchmark_config["models"]
+
+    task_config: dict = tasks_config[TASK]
