eb47c7a3979c4b09035306c7e8cdb132d1f551fd
xReniar <ilaorainer@gmail.com>
2025-08-03 00:17:51 +0200

    Modify qwen2.5-vl parsing function

--- kie_parser.py
+++ kie_parser.py
@@ -35,28 +35,39 @@ def parse_smolvlm(file_name: str):
         json.dump(parsed_predictions, f, indent=4)
 
 
-def parse_qwen2_5():
-    folder_path = "responses/raw/kie/qwen2.5"
-    responses = sorted(os.listdir(folder_path))
+def parse_qwen2_5(file_name: str):
+    folder_path = "responses/raw"
+    predictions: dict = json.load(open(f"{folder_path}/{file_name}.json", "r"))
+    os.makedirs("responses/parsed", exist_ok=True)
 
-    for response in responses:
-        json_response = json.load(open(os.path.join(folder_path, response), "r"))
+    parsed_predictions = {}
 
-        response_field: str = json_response["response"]
+    for img_fn in predictions.keys():
+        prediction = predictions[img_fn]
 
-        prediction_json = {}
+        response: str = prediction["response"]
+        inference_time: float = prediction["inference_time"]
+
         try:
-            prediction = response_field.replace('```json', '').replace('```', '').strip()
-            prediction_json = json.loads(prediction)
+            pred = response.replace('```json', '').replace('```', '').strip()
+            pred = json.loads(pred)
+
+            if not isinstance(pred, dict):
+                raise ValueError("Parsed prediction is not a dictionary")
         except:
-            prediction_json = {}
+            pred = {}
+        finally:
+            parsed_predictions[img_fn] = dict(
+                response = pred,
+                inference_time = inference_time
+            )
 
-        with open(os.path.join(folder_path.replace("raw", "parsed"), response), "w") as f:
-            json.dump(prediction_json, f, indent=4)
+    with open(f"responses/parsed/{file_name}.json", "w") as f:
+        json.dump(parsed_predictions, f, indent=4)
 
 
 
-parse_smolvlm("SmolVLM-500M-kie")
-parse_smolvlm("SmolVLM-kie")
-parse_smolvlm("SmolVLM2-kie")
-#parse_qwen2_5()
+#parse_smolvlm("SmolVLM-500M-kie")
+#parse_smolvlm("SmolVLM-kie")
+#parse_smolvlm("SmolVLM2-kie")
+parse_qwen2_5("qwen2.5-7b")
