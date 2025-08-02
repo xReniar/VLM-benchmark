e9a12e082233c4912f845a7de5e3f41d46f4fc02
xReniar <ilaorainer@gmail.com>
2025-08-02 17:17:14 +0200

    Update vlm_test

--- vlm_test.py
+++ vlm_test.py
@@ -2,28 +2,33 @@ from vllm import LLM
 from PIL import Image
 
 
-vlm = LLM(
-    model="Qwen/Qwen2.5-VL-3B-Instruct-AWQ",
-    quantization="AWQ"
-)
+def main():
+    vlm = LLM(
+        model="Qwen/Qwen2.5-VL-3B-Instruct-AWQ",
+        quantization="AWQ",
+        dtype="float16"
+    )
 
-img = Image.open("data/kie/images/0.png")
-prompt = "perform"
+    img = Image.open("data/kie/images/0.png")
+    prompt = "perform"
 
-conversation = [
-    {"role": "system", "content": "You are a helpful assistant"},
-    {
-        "role": "user",
-        "content": [
-            { "type": "image_pil", "image_pil": img },
-            { "type": "text", "text": prompt }
-        ],
-    },
-]
+    conversation = [
+        {"role": "system", "content": "You are a helpful assistant"},
+        {
+            "role": "user",
+            "content": [
+                { "type": "image_pil", "image_pil": img },
+                { "type": "text", "text": prompt }
+            ],
+        },
+    ]
 
-outputs = vlm.chat(conversation)
+    outputs = vlm.chat(conversation)
 
 
-for o in outputs:
-    generated_text = o.outputs[0].text
-    print(generated_text)
+    for o in outputs:
+        generated_text = o.outputs[0].text
+        print(generated_text)
+
+if __name__ == "__main__":
+    main()
