4469cece34150fa2005eec024bbdd3cb8cf13a10
xReniar <ilaorainer@gmail.com>
2025-08-02 17:17:00 +0200

    Modify smolvlm parser

--- kie_parser.py
+++ kie_parser.py
@@ -3,29 +3,9 @@ import json
 import ast
 
 
-def parse_smolvlm():
-    '''
-    folder_path = "responses/raw/kie/smolvlm"
-    responses = sorted(os.listdir(folder_path))
-
-    for response in responses:
-        json_response = json.load(open(os.path.join(folder_path, response), "r"))
-
-        response_field:str = json_response["prediction"]
-
-        prediction_json = {}
-        try:
-            prediction = response_field.split("Assistant: ")[1]
-            prediction_json = json.loads(prediction)
-        except:
-            pass
-    
-        with open(os.path.join(folder_path.replace("raw", "parsed"), response), "w") as f:
-            json.dump(prediction_json, f, indent=4)
-    '''
+def parse_smolvlm(file_name: str):
     folder_path = "responses/raw"
-    file_name = "SmolVLM-500M-kie.json"
-    predictions: dict = json.load(open(f"{folder_path}/{file_name}", "r"))
+    predictions: dict = json.load(open(f"{folder_path}/{file_name}.json", "r"))
     os.makedirs("responses/parsed", exist_ok=True)
 
     parsed_predictions = {}
@@ -43,19 +23,15 @@ def parse_smolvlm():
 
             if not isinstance(pred, dict):
                 raise ValueError("Parsed prediction is not a dictionary")
-
-
+        except:
+            pred = {}
+        finally:
             parsed_predictions[img_fn] = dict(
                 response = pred,
                 inference_time = inference_time
             )
-        except:
-            parsed_predictions[img_fn] = dict(
-                response = {},
-                inference_time = inference_time
-            )
 
-    with open(f"responses/parsed/{file_name}", "w") as f:
+    with open(f"responses/parsed/{file_name}.json", "w") as f:
         json.dump(parsed_predictions, f, indent=4)
 
 
@@ -80,5 +56,7 @@ def parse_qwen2_5():
 
 
 
-parse_smolvlm()
+parse_smolvlm("SmolVLM-500M-kie")
+parse_smolvlm("SmolVLM-kie")
+parse_smolvlm("SmolVLM2-kie")
 #parse_qwen2_5()
