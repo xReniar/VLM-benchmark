cea1f9a3f4fefeacf5e6301874062564cffc018f
xReniar <ilaorainer@gmail.com>
2025-08-16 22:57:19 +0200

    Update folder_path and output_dir with dataset parameter

--- benchmark.py
+++ benchmark.py
@@ -13,9 +13,8 @@ def inference(
     dataset: str,
     models: list[str]
 ):
-    folder_path = "data/sroie/test/img"
-    dataset_name = "sroie"
-    output_dir = f"responses/raw/{dataset_name}"
+    folder_path = f"data/{dataset}/test/img"
+    output_dir = f"responses/raw/{dataset}"
     os.makedirs(output_dir, exist_ok=True)
 
     #field_names = ["date", "doc_no_receipt_no", "seller_address", "seller_gst_id", "seller_name", "seller_phone", "total_amount", "total_tax"]
