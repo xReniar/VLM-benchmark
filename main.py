9b8268b95aedbed4248ee7d3d95634df3d6ac63c
xReniar <ilaorainer@gmail.com>
2025-08-17 20:44:47 +0200

    Add prompt generator function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from vlm import VLM
+from dataset import Dataset, Data, SROIE, DocILE, Task
 import benchmark
 import torch
 import argparse
@@ -8,34 +9,44 @@ import yaml
 import gc
 
 
+def generate_prompt(task: str, data: Data) -> str:
+    prompt = ""
+    if task == Task.KIE.value:
+        kie_data = data.to_json(task)
+        field_names = list(kie_data.keys())
+        output_format = {field: ".." for field in field_names}
+
+        prompt = "Extract the following {fields} from the above document. If a field is not present, return ''. Return the output in a valid JSON format like {output_format}" \
+            .format(
+                fields = field_names,
+                output_format = output_format
+            )
+
+    if task == Task.OCR.value:
+        print(task)
+    if task == Task.VQA.value:
+        print(task)
+    return prompt
+
+
 def inference(
     task: str,
     dataset: str,
     models: list[str]
 ):
-    folder_path = f"data/{dataset}/test/img"
     output_dir = f"responses/raw/{dataset}"
     os.makedirs(output_dir, exist_ok=True)
-
-    #field_names = ["date", "doc_no_receipt_no", "seller_address", "seller_gst_id", "seller_name", "seller_phone", "total_amount", "total_tax"]
-    field_names = ['address', 'company', 'date', 'total']
-    output_format = {field: ".." for field in field_names}
-
-
-    prompt = "Extract the following {fields} from the above document. If a field is not present, return ''. Return the output in a valid JSON format like {output_format}" \
-        .format(
-            fields = field_names,
-            output_format = output_format
-        )
     
     for model_name in models:
         output_dict = {}
         model = VLM(model_name)
 
-        for fn in sorted(os.listdir(folder_path)):
+        for data in SROIE(tasks=[task], split="test"):
+            fn = data.image_path.split("/")[-1]
+
             output_dict[fn] = model.predict(
-                img_path=f"{folder_path}/{fn}",
-                prompt=prompt
+                img_path=data.image_path,
+                prompt=generate_prompt(data.to_json(task))
             )
 
             with open(f"{output_dir}/{model_name}-{task}.json", "w") as f:
@@ -49,13 +60,15 @@ def inference(
 
 if __name__ == "__main__":
     benchmark_config = yaml.safe_load(open("./configs/benchmark.yaml", "r"))
-    test_config = benchmark_config["test"]
-    tasks_config = benchmark_config["tasks"]
+    test_config: dict = benchmark_config["test"]
 
     TASK: str = test_config["task"]
     DATASET: str = test_config["dataset"]
     MODELS: list[str] = test_config["models"]
 
-    task_config: dict = tasks_config[TASK]
+    #inference(TASK, DATASET, MODELS)
 
-    inference(TASK, DATASET, MODELS)
+    for data in SROIE(tasks=[TASK], split="test"):
+        fn = data.image_path.split("/")[-1]
+
+        print(generate_prompt(TASK, data))
