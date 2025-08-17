bbd5ad98ea60947ce035dc29ba37410d92e1533d
xReniar <ilaorainer@gmail.com>
2025-08-18 00:56:22 +0200

    Create Dataset for KIE nanonets

---
+++ dataset/nnts_kie.py
@@ -0,0 +1,20 @@
+from .ds import Dataset, Data, Task
+from typing import ClassVar
+import json
+
+
+class NNTS_KIE(Dataset):
+    TASKS: ClassVar[list[Task]] = [Task.OCR, Task.KIE]
+
+    def __init__(
+        self,
+        tasks: list[Task],
+        split: str
+    ) -> None:
+        super().__init__(tasks=tasks, split=split)
+
+    def _download() -> None:
+        pass
+
+    def _load_data(self) -> None:
+        pass
