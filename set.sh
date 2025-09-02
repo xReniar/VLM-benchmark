37fe2913976f847925df1b6909674801822212b3
xReniar <ilaorainer@gmail.com>
2025-09-02 20:34:50 +0200

    Remove set.sh for vast.ai

--- set.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt install gcc g++ zip unzip nano
-sudo ln -s /usr/lib/x86_64-linux-gnu/libcuda.so.570.169 /usr/lib/x86_64-linux-gnu/libcuda.so
-sudo ldconfig
-
-export PYTHONPATH=$PYTHONPATH:/home/rainer/Code/VLM-benchmark
