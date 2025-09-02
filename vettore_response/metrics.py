38c9319c76588b77dd5a7c1d04b79d9c78f0cf11
xReniar <ilaorainer@gmail.com>
2025-09-02 20:16:24 +0200

    Add metrics script

---
+++ vettore_response/metrics.py
@@ -0,0 +1,38 @@
+import os
+import json
+from Levenshtein import distance as edit_distance
+
+
+def metrics(fn_path: str, gt_path: str):
+    predictions = json.load(open(fn_path, "r"))
+
+    edit_distances = []
+
+    for img_fn in predictions.keys():
+        prediction: dict = predictions[img_fn]
+        response: dict = prediction["response"]
+
+        gt_fn: str = img_fn.split(".")[0]
+        gt_json: dict = json.load(open(os.path.join(gt_path, f"{gt_fn}.json"), "r"))
+        
+        for gt_field in gt_json.keys():
+            pred_value = response.get(gt_field, "")
+
+            pred_value = str(pred_value)
+            gt_value = str(gt_json[gt_field])
+
+            dist = edit_distance(pred_value, gt_value)
+            max_len = max(len(pred_value), len(gt_value))
+            if max_len == 0:
+                edit_distances.append(1.0)
+            else:
+                edit_distances.append(1 - (dist / max_len))
+
+    return round(sum(edit_distances) / len(edit_distances), 3)
+
+
+dataset = "docile"
+gt_path = f"gt/{dataset}_gt"
+folder_path = f"result-{dataset}/parsed"
+for fn in sorted(os.listdir(folder_path)):
+    print(f"{fn}: {metrics(f'{folder_path}/{fn}', gt_path)}")
