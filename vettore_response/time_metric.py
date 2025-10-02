675fcbdb7ccc80d244a1f3fbb8ad17f8e4b6afd1
xReniar <ilaorainer@gmail.com>
2025-10-02 02:07:00 +0200

    Add time metric script

---
+++ vettore_response/time_metric.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+
+def metric(dataset: str):
+    folder_path = f"result-{dataset}/parsed"
+    files = sorted(os.listdir(folder_path))
+
+    for file in files:
+        file_dict = json.load(open(f"{folder_path}/{file}", mode="r"))
+        
+        mean_time = 0
+        for key in file_dict.keys():
+            obj = file_dict[key]
+
+            mean_time += obj["inference_time"]
+
+        print(file, mean_time / len(file_dict.keys()))
+
+
+
+
+metric("docile")
